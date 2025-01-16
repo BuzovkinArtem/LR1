d01e7506c74eb55f6e8ffc74d20caf341e379b74
Максим Ордян <156433440+Olezenka@users.noreply.github.com>
2025-01-16 12:36:06 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LR1.employee;
+
+namespace LR1
+{
+    internal class Program
+
+    {
+
+        public static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FullName ="Иванов";
+            employee.Salary =63500;
+
+            employee.Show();
+
+            double annualSalary = employee.CalculateAnnualSalary();
+            Console.WriteLine($"Годовой размер зарплаты = {annualSalary}");
+        }
+    }
+}
+
+
---
+++ employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR1
+{
+    internal class employee
+    {
+        public class Employee
+        {
+            public string FullName;
+            public double Salary;
+
+            public void Show()
+            {
+                Console.WriteLine($"ФИО:{FullName}");
+                Console.WriteLine($"Зарплата:{Salary}");
+            }
+
+            public double CalculateAnnualSalary()
+            {
+                return Salary * 12;
+            }
+        }
+    }
+}
+
+        
